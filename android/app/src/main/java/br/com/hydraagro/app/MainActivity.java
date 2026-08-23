6e950b44058ef6617540e6a9a85b4ea1ed917bc3
lima dev <danqxy7@gmail.com>
2026-08-22 22:15:52 -0300

    fix: combinar barras do sistema com o app no Android

--- android/app/src/main/java/br/com/hydraagro/app/MainActivity.java
+++ android/app/src/main/java/br/com/hydraagro/app/MainActivity.java
@@ -1,5 +1,36 @@
 package br.com.hydraagro.app;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        int cream = Color.parseColor("#FAF8F1");
+
+        window.setStatusBarColor(cream);
+        window.setNavigationBarColor(cream);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.setNavigationBarContrastEnforced(false);
+            window.setStatusBarContrastEnforced(false);
+        }
+
+        int flags = window.getDecorView().getSystemUiVisibility();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            flags |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+        }
+        window.getDecorView().setSystemUiVisibility(flags);
+    }
+}
