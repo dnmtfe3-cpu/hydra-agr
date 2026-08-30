fdfdd87999e380d18a4a6dfcc47bef6d45ce0c2b
dnmtfe3-cpu <dnmtfe3@gmail.com>
2026-08-30 19:41:05 -0300

    style: load email code auth polish

--- src/main.tsx
+++ src/main.tsx
@@ -26,6 +26,7 @@ import "./herd-highlight.css";
 import "./herd-weight-history.css";
 import "./public-animal.css";
 import "./authentic-ui.css";
+import "./auth-email-code.css";
 import "./interaction-polish.css";
 import "./features/profile/profile-mobile-fix.css";
 import "./features/profile/profile-ranking-runtime";
