288152159d05ed5ea10bf9be8617b261df69cc57
dnmtfe3-cpu <dnmtfe3@gmail.com>
2026-09-09 11:47:31 -0300

    Enable remote maintenance runtime

--- src/main.tsx
+++ src/main.tsx
@@ -39,6 +39,7 @@ import "./auth-no-carousel.css";
 import "./hydra-dark-final.css";
 import "./auth-landing-native.css";
 import "./auth-signup-native.css";
+import "./maintenance-runtime";
 import { HydraAppShell } from "./components/hydra-app-shell";
 import { setupPushNotifications } from "./services/push-notifications";
 import { renderIosPreviewRoute } from "./ios-preview";
