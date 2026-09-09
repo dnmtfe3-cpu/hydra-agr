f5f1911cec28f290b7d3ff90b5f7568ff3dc5be1
dnmtfe3-cpu <dnmtfe3@gmail.com>
2026-09-08 21:39:09 -0300

    Load native signup polish

--- src/main.tsx
+++ src/main.tsx
@@ -38,6 +38,7 @@ import "./seo-runtime";
 import "./auth-no-carousel.css";
 import "./hydra-dark-final.css";
 import "./auth-landing-native.css";
+import "./auth-signup-native.css";
 import { HydraAppShell } from "./components/hydra-app-shell";
 import { setupPushNotifications } from "./services/push-notifications";
 import { renderIosPreviewRoute } from "./ios-preview";
